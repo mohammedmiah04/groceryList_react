ff0e456868f7a2ac8c186941fc04bc334c517747
MohammedM <nanumiah@nanus-mbp.home>
2021-01-29 09:46:55 +0000

    Updates

---
+++ precache-manifest.8d1f419c3b459774c8ec053b6bc789d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711237efb6f023d9d1c8df7444d2af01",
+    "url": "/groceryList_react/index.html"
+  },
+  {
+    "revision": "14ee613dd887e623d7ed",
+    "url": "/groceryList_react/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "ea1b597a0d05135a845c",
+    "url": "/groceryList_react/static/js/2.9bcf8ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/groceryList_react/static/js/2.9bcf8ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ee613dd887e623d7ed",
+    "url": "/groceryList_react/static/js/main.f954fd07.chunk.js"
+  },
+  {
+    "revision": "b9137ea821cf8dac71b5",
+    "url": "/groceryList_react/static/js/runtime-main.e7fd9726.js"
+  }
+]);
